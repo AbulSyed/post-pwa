972921cd5f6d5a82e5993d9055e0e47709a4c2ec
Abul Syed <syed1234@hotmail.co.uk>
2021-08-04 14:50:07 +0100

    camera

--- src/pages/CameraPage.vue
+++ src/pages/CameraPage.vue
@@ -2,18 +2,33 @@
   <q-page class="q-pa-md constrain-large">
 
     <div class="camera-frame q-pa-md">
-      <q-img
-        src="https://imagesvc.meredithcorp.io/v3/mm/image?q=85&c=sc&poi=face&w=1600&h=800&url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F28%2F2017%2F02%2Feiffel-tower-paris-france-EIFFEL0217.jpg"
-      />
+      <video v-show="!imgCaptured" ref="video" class="full-width" autoplay />
+      <canvas v-show="imgCaptured" ref="canvas" class="full-width" height="240" />
     </div>
     <div class="text-center q-pa-md">
       <q-btn
+        v-if="cameraEnabled"
+        @click="captureImg"
         icon="camera"
         size="lg"
         dense
         flat
         round
       />
+      <div class="row justify-center">
+        <q-file
+          v-if="!cameraEnabled"
+          v-model="imgUpload"
+          @update:model-value="captureImgFallback"
+          accept="image/*"
+          label="Select an image"
+          outlined
+        >
+          <template v-slot:prepend>
+            <q-icon name="attach_file" />
+          </template>
+        </q-file>
+      </div>
       <div class="row justify-center q-pa-md">
         <q-input
           v-model="post.caption"
@@ -34,7 +49,7 @@
         </q-input>
       </div>
       <div class="row justify-center q-pa-md">
-        <q-btn flat dense round icon="add" size="lg" @click="handleClick" />
+        <q-btn flat dense round icon="add" size="lg" />
       </div>
     </div>
 
@@ -53,14 +68,70 @@ export default {
         location: '',
         photo: null,
         date: Date.now()
-      }
+      },
+      imgCaptured: false,
+      imgUpload: [],
+      cameraEnabled: true
     }
   },
   methods: {
-    handleClick() {
-      console.log(this.post)
+    initCamera(){
+      navigator.mediaDevices.getUserMedia({
+        video: true
+      }).then(stream => {
+        console.log(this.$refs.video)
+        this.$refs.video.srcObject = stream
+      }).catch(err => {
+        this.cameraEnabled = false
+      })
+    },
+    captureImg(){
+      let video = this.$refs.video
+      let canvas = this.$refs.canvas
+
+      canvas.width = video.getBoundingClientRect().width
+      canvas.height = video.getBoundingClientRect().height
+
+      let context = canvas.getContext('2d')
+      context.drawImage(video, 0, 0, canvas.width, canvas.height)
+      this.imgCaptured = true
+      
+      canvas.toBlob(blob => (this.post.photo = blob))
+      this.disableCamera()
+    },
+    disableCamera(){
+      this.$refs.video.srcObject.getVideoTracks().forEach(track => {
+        track.stop()
+      })
+    },
+    captureImgFallback(file){
+      this.post.photo = file
+
+      let canvas = this.$refs.canvas
+      let context = canvas.getContext('2d')
+
+      var reader = new FileReader()
+      reader.onload = e => {
+        var img = new Image()
+        img.onload = () => {
+          canvas.width = img.width
+          canvas.height = img.height
+          context.drawImage(img,0,0)
+          this.imgCaptured = true
+        }
+        img.src = e.target.result
+      }
+      reader.readAsDataURL(file)
     }
   },
+  mounted(){
+    this.initCamera()
+  },
+  beforeUnmount(){
+    if(this.cameraEnabled){
+      this.disableCamera()
+    }
+  }
 }
 </script>
 
