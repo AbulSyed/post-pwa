014b01f21972d0817c91173f15d90ebebaca7586
Abul Syed <syed1234@hotmail.co.uk>
2021-09-03 15:10:11 +0100

    saving image with post

--- backend/src/routes/post.js
+++ backend/src/routes/post.js
@@ -7,6 +7,14 @@ const router = new express.Router();
 */
 const inspect = require('util').inspect;
 const Busboy = require('busboy');
+/*
+  -> Using nodejs temp folder to store file temporarily
+  -> Upload img to firebase storage from temp folder
+*/
+const path = require('path');
+const os = require('os'); // allows access the temp folder
+const fs = require('fs'); // allows to write file to temp folder
+const { v4 } = require('uuid');
 
 /*
   Cloud firestore connection
@@ -23,10 +31,12 @@ const admin = require('firebase-admin');
 const serviceAccount = require('../serviceAccountKey.json');
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount),
+  storageBucket: 'quasar-pwa-960cc.appspot.com'
 });
 
 const db = admin.firestore();
+const bucket = admin.storage().bucket();
 
 router.get('/posts', async (req, res) => {
   res.set('Access-Control-Allow-Origin', '*');
@@ -46,21 +56,20 @@ router.get('/posts', async (req, res) => {
 router.post('/posts', async (req, res) => {
   res.set('Access-Control-Allow-Origin', '*');
 
+  const uuid = v4();
+
   var busboy = new Busboy({ headers: req.headers });
 
   const fields = {};
+  let fileData = {};
 
   // on file hook trigerred on every FILE sent to this endpoint
   busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
     console.log('File [' + fieldname + ']: filename: ' + filename + ', encoding: ' + encoding + ', mimetype: ' + mimetype);
-    // fires when busboy recieves data of formdata
-    file.on('data', function(data) {
-      console.log('File [' + fieldname + '] got ' + data.length + ' bytes');
-    });
-    // fires when busboy finished grabbing all formdata
-    file.on('end', function() {
-      console.log('File [' + fieldname + '] Finished');
-    });
+    
+    const filepath = path.join(os.tmpdir(), filename);
+    file.pipe(fs.createWriteStream(filepath));
+    fileData = { filepath, mimetype };
   });
 
   // on file hook trigerred on every FIELD sent to this endpoint
@@ -71,19 +80,42 @@ router.post('/posts', async (req, res) => {
 
   // fires when busboy finished parsing form
   busboy.on('finish', async function() {
-    // save form data to firebase firestore
-    const docRef = db.collection('posts').doc(fields.id);
+    // upload image first then get url and save to db
+    bucket.upload(
+      fileData.filepath,
+      {
+        uploadType: 'media',
+        metadata: {
+          metadata: {
+            contentType: fileData.mimetype,
+            firebaseStorageDownloadTokens: uuid
+          }
+        }
+      }, (err, uploadedFile) => {
+        if(!err) {
+          uploadDocument(uploadedFile);
+        }
+      }
+    );
 
-    await docRef.set({
-      id: fields.id,
-      caption: fields.caption,
-      location: fields.location,
-      timestamp: parseInt(fields.timestamp),
-      imgUrl: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/spiderman-1604616315.jpg?crop=0.5xw:1xh;center,top&resize=640:*'
-    });
+    const uploadDocument = async (uploadedFile) => {
+      // save form data to firebase firestore
+      const docRef = db.collection('posts').doc(fields.id);
+  
+      try {
+        await docRef.set({
+          id: fields.id,
+          caption: fields.caption,
+          location: fields.location,
+          timestamp: parseInt(fields.timestamp),
+          imgUrl: `https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/${uploadedFile.name}?alt=media&token=${uuid}`
+        });
 
-    console.log('Done parsing form!');
-    res.send('Done parsing form');
+        res.send('Done parsing form');
+      }catch(err) {
+        console.log(err);
+      }
+    }
   });
 
   req.pipe(busboy);
